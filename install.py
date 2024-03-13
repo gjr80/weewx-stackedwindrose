7b5608d5cd90d0c59b97bb3363e134c73bfdf75b
gary <gary@therodericks.id.au>
2024-03-14 07:21:00 +1000

    - remove distutils.StrictVersion dependency - use weecfg.extension.ExtensionInstaller in lieu of deprecated setup.ExtensionInstaller - use a multi-line string and StringIO to obtain the extension installer config dict

--- install.py
+++ install.py
@@ -10,9 +10,14 @@ PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
 Installer for StackedWindRose Image Generator Extension
 
-Version: 3.0.2                                      Date: 6 July 2023
+Version: 3.1.0                                      Date: 14 March 2024
 
 Revision History
+    14 March 2024
+        -   removed distutils.StrictVersion dependency
+        -   now uses weecfg.extension.ExtensionInstaller
+        -   now uses a multi-line string and StringIO to obtain the extension
+            installer config dict
     6 July 2023         v3.0.2
         -   version change only
     7 June 2020         v3.0.1
@@ -41,24 +46,100 @@ Revision History
 """
 
 # python imports
-from distutils.version import StrictVersion
+import configobj
+from weecfg.extension import ExtensionInstaller
+
+# import StringIO, use six.moves due to python2/python3 differences
+from six.moves import StringIO
 
 # WeeWX imports
 import weewx
 
-from setup import ExtensionInstaller
+# note do not use release candidates, eg 5.0.0rc3
+REQUIRED_WEEWX_VERSION = "3.2.0"
+STACKEDWINDROSE_VERSION = "3.1.0"
 
-REQUIRED_VERSION = "3.2.0"
-STACKEDWINDROSE_VERSION = "3.0.2"
+# define our config as a multiline string so we can preserve comments
+stacked_config = """
+[StdReport]
+    [[StackedWindRose]]
+        skin = StackedWindRose
+        [[[Units]]]
+            [[[[Groups]]]]
+                group_speed = km_per_hour
+            [[[[Labels]]]]
+                km_per_hour = km/h
+                knot = knots
+                meter_per_second = m/s
+                mile_per_hour = mph
+        [[[Labels]]]
+            compass_points = N, S, E, W
+            [[[[Generic]]]]
+                windGust = Gust Speed
+                windSpeed = Wind Speed
+        [[[StackedWindRoseImageGenerator]]]
+            image_background_image = None
+            image_width = 382
+            image_height = 361
+            image_background_circle_color = 0xF5F5F5
+            image_background_box_color = 0xF5C696
+            image_background_range_ring_color = 0xC3D9DD
+            windrose_plot_border = 5
+            windrose_legend_bar_width = 10
+            windrose_font_path = /usr/share/fonts/truetype/freefont/FreeSansBold.ttf
+            windrose_plot_font_size = 10
+            windrose_plot_font_color = 0x000000
+            windrose_legend_font_size = 10
+            windrose_legend_font_color = 0x000000
+            windrose_label_font_size = 12
+            windrose_label_font_color = 0x000000
+            windrose_plot_petal_colors = aqua, 0xFF9900, 0xFF3300, 0x009900, 0x00CC00, 0x33FF33, 0x00FFCC
+            windrose_plot_petal_width = 16
+            [[[[day_images]]]]
+                time_length = 86400
+                [[[[[daywindrose]]]]]
+                    format = png
+                    [[[[[[windSpeed]]]]]]
+                        label = 24 Hour Wind Rose
+                        time_stamp = %H:%M %-d %b %y
+                        time_stamp_location = bottom, right
+"""
+
+# construct our config dict
+stacked_dict = configobj.ConfigObj(StringIO(stacked_config))
+
+def version_compare(v1, v2):
+    """Basic 'distutils' and 'packaging' free version comparison.
+
+    v1 and v2 are WeeWX version numbers in string format.
+
+    Returns:
+        0 if v1 and v2 are the same
+        -1 if v1 is less than v2
+        +1 if v1 is greater than v2
+    """
+
+    import itertools
+    mash = itertools.zip_longest(v1.split('.'), v2.split('.'), fillvalue='0')
+    for x1, x2 in mash:
+        try:
+            y1, y2 = int(x1), int(x2)
+        except ValueError:
+            y1, y2 = x1, x2
+        if y1 > y2:
+            return 1
+        if y1 < y2:
+            return -1
+    return 0
 
 def loader():
     return StackedWindRoseInstaller()
 
 class StackedWindRoseInstaller(ExtensionInstaller):
     def __init__(self):
-        if StrictVersion(weewx.__version__) < StrictVersion(REQUIRED_VERSION):
-            msg = "%s requires WeeWX %s or greater, found %s" % (''.join(('StackedWindRose ', STACKEDWINDROSE_VERSION)),
-                                                                 REQUIRED_VERSION,
+        if version_compare(weewx.__version__, REQUIRED_WEEWX_VERSION) < 0:
+            msg = "%s requires WeeWX %s or greater, found %s" % (' '.join(('StackedWindRose', STACKEDWINDROSE_VERSION)),
+                                                                 REQUIRED_WEEWX_VERSION,
                                                                  weewx.__version__)
             raise weewx.UnsupportedFeature(msg)
         super(StackedWindRoseInstaller, self).__init__(
@@ -67,63 +148,7 @@ class StackedWindRoseInstaller(ExtensionInstaller):
             description='Stacked windrose image generator for WeeWX.',
             author="Gary Roderick",
             author_email="gjroderick@gmail.com",
-            config={
-                'StdReport': {
-                    'StackedWindRose': {
-                        'skin': 'StackedWindRose',
-                        'Units': {
-                            'Groups': {
-                                'group_speed': 'km_per_hour'
-                            },
-                            'Labels': {
-                                'km_per_hour': 'km/h',
-                                'knot': 'knots',
-                                'meter_per_second': 'm/s',
-                                'mile_per_hour': 'mph'
-                            },
-                        },
-                        'Labels': {
-                            'compass_points': ['N', 'S', 'E', 'W'],
-                            'Generic': {
-                                'windGust': 'Gust Speed',
-                                'windSpeed': 'Wind Speed'
-                            }
-                        },
-                        'StackedWindRoseImageGenerator': {
-                            'image_background_image': 'None',
-                            'image_width': '382',
-                            'image_height': '361',
-                            'image_background_circle_color': '0xF5F5F5',
-                            'image_background_box_color': '0xF5C696',
-                            'image_background_range_ring_color': '0xC3D9DD',
-                            'windrose_plot_border': '5',
-                            'windrose_legend_bar_width': '10',
-                            'windrose_font_path': '/usr/share/fonts/truetype/freefont/FreeSansBold.ttf',
-                            'windrose_plot_font_size': '10',
-                            'windrose_plot_font_color': '0x000000',
-                            'windrose_legend_font_size': '10',
-                            'windrose_legend_font_color': '0x000000',
-                            'windrose_label_font_size': '12',
-                            'windrose_label_font_color': '0x000000',
-                            'windrose_plot_petal_colors': ['aqua', '0xFF9900', '0xFF3300', '0x009900', '0x00CC00', '0x33FF33', '0x00FFCC'],
-                            'windrose_plot_petal_width': '16',
-                            'day_images': {
-                                'time_length': '86400',
-                                'daywindrose': {
-                                    'format': 'png',
-                                    'windSpeed': {
-                                        'label': '24 Hour Wind Rose',
-                                        'time_stamp': '%H:%M %-d %b %y',
-                                        'time_stamp_location': ['bottom', 'right']
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            files=[
-                ('bin/user', ['bin/user/stackedwindrose.py']),
-                ('skins/StackedWindRose', ['skins/StackedWindRose/skin.conf'])
-            ]
+            files=[('bin/user', ['bin/user/stackedwindrose.py']),
+                   ('skins/StackedWindRose', ['skins/StackedWindRose/skin.conf'])],
+            config=stacked_dict
         )
